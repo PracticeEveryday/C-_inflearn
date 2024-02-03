8e44a4426e41239250fc846518cfdee71781722e
PracticeEveryday <developerforus@gmail.com>
2024-02-03 10:24:18 +0900

    auto 자동 추론

---
+++ study_book/auto.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  int a = 1;
+  auto b = 1;
+
+  cout << b << '\n';
+
+  vector<pair<int, int>> v;
+
+  for(int i = 1; i <=5; i++) {
+    v.push_back({i, i});
+  }
+
+  for(auto it: v) {
+    cout << it.first << " : " << it.second << "\n";
+  }
+
+  for(pair<int, int> it: v) {
+    cout << it.first << " : " << it.second << "\n";
+  }
+
+  return 0;
+}
