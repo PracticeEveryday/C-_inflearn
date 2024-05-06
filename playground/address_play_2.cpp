7f4c9ac47bf3341ebf850c4e126e9f7055051779
PracticeEveryday <developerforus@gmail.com>
2024-05-06 22:30:22 +0900

    feat: playground

---
+++ playground/address_play_2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+int main() {
+  init();
+
+  
+
+
+  return 0;
+}
